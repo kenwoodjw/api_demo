e06c99010d89e0ba5e0c6745a3698e90ff990086
kenwoodjw <blackxin55+@gmail.com>
2024-09-25 10:38:14 +0800

    update app.py use turn
    Signed-off-by: kenwoodjw <blackxin55+@gmail.com>

--- app.py
+++ app.py
@@ -8,7 +8,7 @@ import uuid
 
 import cv2
 from aiohttp import web
-from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription
+from aiortc import MediaStreamTrack, RTCPeerConnection, RTCSessionDescription, RTCIceServer, RTCConfiguration
 from aiortc.contrib.media import MediaBlackhole, MediaPlayer, MediaRecorder, MediaRelay
 from av import VideoFrame
 
@@ -18,12 +18,8 @@ logger = logging.getLogger("pc")
 pcs = set()
 relay = MediaRelay()
 
-
+# 视频处理类
 class VideoTransformTrack(MediaStreamTrack):
-    """
-    A video stream track that transforms frames from an another track.
-    """
-
     kind = "video"
 
     def __init__(self, track, transform):
@@ -36,14 +32,11 @@ class VideoTransformTrack(MediaStreamTrack):
 
         if self.transform == "cartoon":
             img = frame.to_ndarray(format="bgr24")
-
-            # prepare color
             img_color = cv2.pyrDown(cv2.pyrDown(img))
             for _ in range(6):
                 img_color = cv2.bilateralFilter(img_color, 9, 9, 7)
             img_color = cv2.pyrUp(cv2.pyrUp(img_color))
 
-            # prepare edges
             img_edges = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
             img_edges = cv2.adaptiveThreshold(
                 cv2.medianBlur(img_edges, 7),
@@ -54,33 +47,26 @@ class VideoTransformTrack(MediaStreamTrack):
                 2,
             )
             img_edges = cv2.cvtColor(img_edges, cv2.COLOR_GRAY2RGB)
-
-            # combine color and edges
             img = cv2.bitwise_and(img_color, img_edges)
 
-            # rebuild a VideoFrame, preserving timing information
             new_frame = VideoFrame.from_ndarray(img, format="bgr24")
             new_frame.pts = frame.pts
             new_frame.time_base = frame.time_base
             return new_frame
         elif self.transform == "edges":
-            # perform edge detection
             img = frame.to_ndarray(format="bgr24")
             img = cv2.cvtColor(cv2.Canny(img, 100, 200), cv2.COLOR_GRAY2BGR)
 
-            # rebuild a VideoFrame, preserving timing information
             new_frame = VideoFrame.from_ndarray(img, format="bgr24")
             new_frame.pts = frame.pts
             new_frame.time_base = frame.time_base
             return new_frame
         elif self.transform == "rotate":
-            # rotate image
             img = frame.to_ndarray(format="bgr24")
             rows, cols, _ = img.shape
             M = cv2.getRotationMatrix2D((cols / 2, rows / 2), frame.time * 45, 1)
             img = cv2.warpAffine(img, M, (cols, rows))
 
-            # rebuild a VideoFrame, preserving timing information
             new_frame = VideoFrame.from_ndarray(img, format="bgr24")
             new_frame.pts = frame.pts
             new_frame.time_base = frame.time_base
@@ -88,22 +74,31 @@ class VideoTransformTrack(MediaStreamTrack):
         else:
             return frame
 
-
+# Serve index.html
 async def index(request):
     content = open(os.path.join(ROOT, "index.html"), "r").read()
     return web.Response(content_type="text/html", text=content)
 
-
+# Serve client.js
 async def javascript(request):
     content = open(os.path.join(ROOT, "client.js"), "r").read()
     return web.Response(content_type="application/javascript", text=content)
 
-
+# Handle WebRTC offer
 async def offer(request):
     params = await request.json()
     offer = RTCSessionDescription(sdp=params["sdp"], type=params["type"])
 
-    pc = RTCPeerConnection()
+    # 使用 TURN 服务器并强制 TCP 传输
+    
+   
+    turn_urls='stun:stun.l.google.com:19302?transport=tcp' 
+    
+
+    
+
+    # 创建 PeerConnection，使用 TURN 服务器
+    pc = RTCPeerConnection(RTCConfiguration(iceServers=[RTCIceServer(urls=turn_urls)]))
     pc_id = "PeerConnection(%s)" % uuid.uuid4()
     pcs.add(pc)
 
@@ -112,7 +107,7 @@ async def offer(request):
 
     log_info("Created for %s", request.remote)
 
-    # prepare local media
+    # Prepare local media
     player = MediaPlayer(os.path.join(ROOT, "demo-instruct.wav"))
     if args.record_to:
         recorder = MediaRecorder(args.record_to)
@@ -154,11 +149,11 @@ async def offer(request):
             log_info("Track %s ended", track.kind)
             await recorder.stop()
 
-    # handle offer
+    # Handle offer
     await pc.setRemoteDescription(offer)
     await recorder.start()
 
-    # send answer
+    # Create answer and set as local description
     answer = await pc.createAnswer()
     await pc.setLocalDescription(answer)
 
@@ -169,14 +164,14 @@ async def offer(request):
         ),
     )
 
-
+# Gracefully shutdown connections
 async def on_shutdown(app):
-    # close peer connections
+    # Close peer connections
     coros = [pc.close() for pc in pcs]
     await asyncio.gather(*coros)
     pcs.clear()
 
-
+# Main server application
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         description="WebRTC audio / video / data-channels demo"
